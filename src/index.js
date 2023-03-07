3d427fa01fc44965f35970b35c59270153c1f6dd
Yury Baykov <y.baykov@corp.mail.ru>
2023-03-07 21:23:48 +0300

    render preview in separate function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const uploadButton = document.querySelector('#upload');
-const previewImg = document.querySelector('#preview');
 
 uploadButton?.addEventListener('change', (event) => {
 	const [file] = event.target.files;
+
+	renderPreview(file);
+});
+
+function renderPreview(file) {
+	const previewImg = document.querySelector('#preview');
 
 	const reader = new FileReader();
 	reader.onload = () => {
 		previewImg.src = reader.result;
 	};
 	reader.readAsDataURL(file);
-});
+}
